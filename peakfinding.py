0fade7102c220a8beec7d7663a2276b0f81a6514
s-aparna <130048896+s-aparna@users.noreply.github.com>
2023-04-08 10:42:12 +0530

    Add files via upload

---
+++ peakfinding.py
@@ -0,0 +1,19 @@
+def findPeakElement(nums):
+        n=len(nums)
+        print(n)
+        for i in range(n-1):
+            if i==0 and nums[i]>nums[i+1]:
+                return i
+
+            if nums[i]>nums[i+1] and nums[i]>nums[i-1]:
+                return i
+            if i==n and nums[i]>nums[i-1]:
+                return i
+
+
+
+
+#print(findPeakElement([1,2,3,1]))
+#nums = [5,2,1,3,5,6,4]
+print(findPeakElement([1,2]))
+print()
